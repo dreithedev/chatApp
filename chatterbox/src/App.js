2ad3e2acfcd74bc5f91a4a486c7c054bbffa1a1d
dreithedev <dreithedev@gmail.com>
2020-09-27 13:45:14 -0500

    Updated App.js to include firebase configuration

--- chatterbox/src/App.js
+++ chatterbox/src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+
+    apiKey: "AIzaSyBZLgKfEmW2HIcrG9T6Xqu3mbL7s6PW-Jc",
+    authDomain: "chatterbox-e274c.firebaseapp.com",
+    databaseURL: "https://chatterbox-e274c.firebaseio.com",
+    projectId: "chatterbox-e274c",
+    storageBucket: "chatterbox-e274c.appspot.com",
+    messagingSenderId: "996488157905",
+    appId: "1:996488157905:web:3dc74d6da504ab0c7403f0",
+    measurementId: "G-LG33GXRCCN"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
